ffec3b3e23c7f04bc8d25e751bb4fd932b62c43e
Pjiwm <pim.munne@gmail.com>
2024-01-19 01:14:12 +0100

    built query object allowing swim times

--- api/src/graphql/query/mod.rs
+++ api/src/graphql/query/mod.rs
@@ -3,4 +3,4 @@ pub mod competition;
 pub mod swimmer;
 pub mod swim_time;
 #[derive(async_graphql::MergedObject, Default)]
-pub struct Query(team::TeamQuery, competition::CompetitionQuery, swimmer::SwimmerQuery);
+pub struct Query(team::TeamQuery, competition::CompetitionQuery, swimmer::SwimmerQuery, swim_time::SwimTimeQuery);
