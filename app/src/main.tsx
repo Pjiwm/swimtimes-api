7af92e17bb3fec2ef5cdd1b5b9e06196e034cd7b
Pjiwm <pim.munne@gmail.com>
2024-01-27 18:43:59 +0100

    apolla init

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,8 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import config from '../app.config.ts';
 import './index.css'
 import './styles/tailwind.css'
+
+const client = new ApolloClient({
+
+  uri: `${config.apiUrl}/api/graphql`,
+
+  cache: new InMemoryCache(),
+
+});
+
+client.query({
+  query: gql`
+    query {
+      getTeamsByName(name: "")
+    }
+  `
+}).then(result => console.log(result));
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
