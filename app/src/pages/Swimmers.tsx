5f8f4aec2d75dffa85e6bfd5605a3d77ce24d018
Pjiwm <pim.munne@gmail.com>
2024-01-28 23:08:42 +0100

    icons on swimmers

--- app/src/pages/Swimmers.tsx
+++ app/src/pages/Swimmers.tsx
@@ -1,40 +1,47 @@
 import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpinner, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSpinner, faSearch, faUser, faUserCircle, faUsers, faCalendar } from '@fortawesome/free-solid-svg-icons';
 import { GET_SWIMMERS_BY_NAME } from '../queries';
 
 const Swimmer: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState('');
-  const [searchTermOnSearch, setSearchTermOnSearch] = useState('');
   const [currentPage, setCurrentPage] = useState(1);
 
-  const { loading, error, data, refetch: _ } = useQuery(
+  const { loading, error, data, refetch } = useQuery(
     GET_SWIMMERS_BY_NAME,
     {
-      variables: { name: searchTermOnSearch, index: (currentPage - 1) },
+      variables: { name: searchTerm, index: (currentPage - 1) },
     }
   );
 
   const handleSearch = () => {
     setCurrentPage(1);
-    setSearchTermOnSearch(searchTerm);
+    refetch({ name: searchTerm }); // Update the query with the current search term
+  };
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
   };
 
   if (loading) return <div className="text-center mt-8"><FontAwesomeIcon icon={faSpinner} spin size="2x" className="text-white" /></div>;
   if (error) return <div className="text-center mt-8">Error: {error.message}</div>;
 
-  let swimmers = data.getSwimmersByName;
+  // Check if data exists before accessing properties
+  const swimmers = data ? data.getSwimmersByName : [];
 
   return (
     <div className="max-w-screen-xl mx-auto mt-8">
       {/* Search bar at the top */}
-      <div className="p-1 bg-gray-100 mb-4 flex items-center rounded-md">
+      <div className="p-4 bg-gray-100 mb-4 flex items-center">
         <input
           type="text"
           value={searchTerm}
           placeholder="Search by swimmer name"
           className="p-4 border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300 flex-grow"
+          onKeyPress={handleKeyPress}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
         <button
@@ -47,26 +54,27 @@ const Swimmer: React.FC = () => {
 
       {/* Swimmers List */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {swimmers.length === 0 ? (
-          <h1 className="text-4xl font-bold mb-4 text-white">No Swimmers found</h1>
-
-        ) : (
-          swimmers.map((swimmer: any, index: number) => (
-            <div key={index} className="bg-white shadow-md p-4 rounded-lg mb-4">
-              <div className="flex flex-col items-start justify-between h-full">
-                <div>
-                  <p className="text-lg font-semibold text-blue-700">ID: {swimmer.id}</p>
-                  <p className="text-lg font-semibold text-blue-700">Name: {swimmer.name}</p>
-                  <p className="text-lg font-semibold text-blue-700">Team: {swimmer.team}</p>
-                  <p className="text-lg font-semibold text-blue-700">
-                    Date of Birth: {new Date(swimmer.dateOfBirth).toLocaleDateString()}
-                  </p>
-                </div>
-                <hr className="my-2" />
+        {swimmers.map((swimmer: any, index: number) => (
+          <div key={index} className="bg-white shadow-md p-4 rounded-lg mb-4">
+            <div className="flex flex-col items-start justify-between h-full">
+              <div>
+                <p className="text-lg font-semibold text-blue-700">
+                  <FontAwesomeIcon icon={faUser} /> ID: {swimmer.id}
+                </p>
+                <p className="text-lg font-semibold text-blue-700">
+                  <FontAwesomeIcon icon={faUserCircle} /> Name: {swimmer.name}
+                </p>
+                <p className="text-lg font-semibold text-blue-700">
+                  <FontAwesomeIcon icon={faUsers} /> Team: {swimmer.team}
+                </p>
+                <p className="text-lg font-semibold text-blue-700">
+                  <FontAwesomeIcon icon={faCalendar} /> Date of Birth: {new Date(swimmer.dateOfBirth).toLocaleDateString('en-US')}
+                </p>
               </div>
+              <hr className="my-2" />
             </div>
-          ))
-        )}
+          </div>
+        ))}
       </div>
     </div>
   );
